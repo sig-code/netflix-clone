800404d4be6b619412619be25d2c109b0e6260b8
takuya-mashimo <m.takku29@gmail.com>
2021-12-31 21:40:24 +0900

    バナーコンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import "./App.css";
 import { requests } from "./request";
-import { Row } from "./Row";
+import { Row } from "./components/Row";
+import { Banner } from "./components/Banner";
 
 function App() {
   console.log(requests.feachNetflixOriginals);
   return (
-    <>
-      <div className='App'>
-        <Row title='NETFLIX ORIGINALS' fetchUrl={requests.feactTopRated} isLargeRow />
-        <Row title='Top Rated' fetchUrl={requests.feactTopRated} />
-        <Row title='Action Movies' fetchUrl={requests.feactActionMovies} />
-        <Row title='Comedy Movies' fetchUrl={requests.feactComedyMovies} />
-        <Row title='Horror Movies' fetchUrl={requests.feactHorrorMovies} />
-        <Row title='Romance Movies' fetchUrl={requests.feactRomanceMovies} />
-        <Row
-          title='Documentaries Movies'
-          fetchUrl={requests.feactDocumentMovies}
-        />
-      </div>
-    </>
+    <div className='App'>
+      <Banner />
+      <Row
+        title='NETFLIX ORIGINALS'
+        fetchUrl={requests.feactTopRated}
+        isLargeRow
+      />
+      <Row title='Top Rated' fetchUrl={requests.feactTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.feactActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.feactComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.feactHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.feactRomanceMovies} />
+      <Row
+        title='Documentaries Movies'
+        fetchUrl={requests.feactDocumentMovies}
+      />
+    </div>
   );
 }
 
